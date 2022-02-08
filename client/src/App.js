0e1180bfa3fbb3154c4215b9d5ad0b5f2e132d0a
Jacob Graham <jakegraham54@gmail.com>
2022-02-08 12:10:30 -0500

    Added component code in App.js which will render components onto the main application screen. Created empty components for showing a student and creating a student. Added generic styles for all components to use

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Container maxWidth="lg">
+        <AppBar className={classes.appBar}>
+          
+        </AppBar>
+      </Container>
     </div>
   );
 }
