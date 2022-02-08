9fcd2522d3cc0164dcb8bb5cc31bd570f91bcf1a
Jacob Graham <jakegraham54@gmail.com>
2022-02-08 10:38:28 -0500

    Removed boilerplate code from App.js in the client-side. Removed React strict mode from index.js which removes extra tools for generating warnings on potential problems, vulernabilities, best practices violations, etc. in an application. Installed material-ui node module which is a library that allows us to import and use different components to create a user interface in our React applications

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <h1>SkillsBuild training</h1>
-      </header>
+      
     </div>
   );
 }
